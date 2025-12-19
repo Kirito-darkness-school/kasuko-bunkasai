5d7c54b41e983935ba6650e5e8ecb7bcef6996a1
Manmoth <129261117+manmoth-24@users.noreply.github.com>
2025-12-19 12:33:50 +0900

    js animation　調整

--- javascripts/animations.js
+++ javascripts/animations.js
@@ -1,16 +1,11 @@
-const crowdedSiteInvite = document.querySelector('nav.invite-box a');
+const crowdedSiteInvite = document.querySelectorAll('nav.invite-box a');
 
-crowdedSiteInvite.forEach((elem)=>
-    {
-        elem.addEventListener('mouseover', () => {
-        const crowdedSiteInviteH3 = crowdedSiteInvite.querySelector('h3');
-        crowdedSiteInviteH3.style.fontSize = '3em';
-    })
-});
+crowdedSiteInvite.forEach((elem)=>{addEventListener('mouseover', () => {
+    const crowdedSiteInviteH3 = elem.querySelector('h3');
+    crowdedSiteInviteH3.style.fontSize = '3em';
+})});
 
-crowdedSiteInvite.forEach((elem)=>{
-    elem.addEventListener('mouseout', ()=>{
-        const crowdedSiteInviteH3 = crowdedSiteInvite.querySelector('h3');
-        crowdedSiteInviteH3.style.fontSize = '2em';
-    })
-})
+crowdedSiteInvite.forEach((elem)=>{addEventListener('mouseout', () => {
+    const crowdedSiteInviteH3 = elem.querySelector('h3');
+    crowdedSiteInviteH3.style.fontSize = '2em';
+})});
