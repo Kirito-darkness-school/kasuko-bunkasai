c79139d4265defd233738e0f12813cabd51e919c
Manmoth <129261117+manmoth-24@users.noreply.github.com>
2025-12-19 12:35:20 +0900

    js animation 調整

--- javascripts/animations.js
+++ javascripts/animations.js
@@ -1,11 +1,13 @@
 const crowdedSiteInvite = document.querySelectorAll('nav.invite-box a');
 
-crowdedSiteInvite.forEach((elem)=>{addEventListener('mouseover', () => {
+crowdedSiteInvite.forEach((elem)=>{
+    elem.addEventListener('mouseover', () => {
     const crowdedSiteInviteH3 = elem.querySelector('h3');
     crowdedSiteInviteH3.style.fontSize = '3em';
 })});
 
-crowdedSiteInvite.forEach((elem)=>{addEventListener('mouseout', () => {
+crowdedSiteInvite.forEach((elem)=>{
+    elem.addEventListener('mouseout', () => {
     const crowdedSiteInviteH3 = elem.querySelector('h3');
     crowdedSiteInviteH3.style.fontSize = '2em';
 })});
