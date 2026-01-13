7c6aaade9474e5ff1861f3a4cd7277e2ff9dca11
Manmoth24 <pc.you2480@gmail.com>
2026-01-13 20:00:50 +0900

    headerを時間差で表示

--- javascripts/animations.js
+++ javascripts/animations.js
@@ -11,3 +11,8 @@ crowdedSiteInvite.forEach((elem)=>{
     const crowdedSiteInviteH3 = elem.querySelector('h3');
     crowdedSiteInviteH3.style.fontSize = '2em';
 })});
+
+setTimeout(() => {
+    const header = document.querySelector('.fixed-header');
+    header.style.transform = 'translateY(0)';
+}, 5000);
